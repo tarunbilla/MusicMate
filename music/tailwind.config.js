efd2979271c23f704719f7adea8a4933f329bc14
tarunbilla <billatarun6@gmail.com>
2025-01-03 23:28:52 +0530

    Added dist folder

--- music/tailwind.config.js
+++ music/tailwind.config.js
@@ -1,12 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./src/**/*.jsx", "./public/index.html"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
